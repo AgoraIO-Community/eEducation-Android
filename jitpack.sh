7015af2f31a071316e72615924b0b2bb6168c8bf
陈镜吾 <chenjingwu@agora.io>
2021-01-12 16:43:47 +0800

    commitMsg

--- jitpack.sh
+++ jitpack.sh
@@ -1,4 +1,22 @@
 #!/bin/zsh
-echo $1
-git commit -a -m $1
+
+commitMsg=$1
+releaseTag=$2
+if [ -z "$commitMsg" ]
+  then
+    echo "commitMsg is empty!"
+    exit 0
+fi
+if [ -z "$releaseTag" ]
+  then
+    echo "releaseTag is empty!"
+    exit 0
+fi
+# shellcheck disable=SC2006
+echo "-----push code to`git branch --show-current`branch----"
+git commit -a -m commitMsg
 git push origin android-sdk:android-sdk
+echo "-----push code success----"
+echo "-----create new release->$releaseTag----"
+git tag -v "$releaseTag" -m "$commitMsg"
+git push origin "$releaseTag"

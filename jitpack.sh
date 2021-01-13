a758e7245da09de47f3864748e524bcd734ae22f
陈镜吾 <chenjingwu@agora.io>
2021-01-13 20:15:11 +0800

    commitMsg

--- jitpack.sh
+++ jitpack.sh
@@ -17,16 +17,35 @@ echo "-----push code to `git branch --show-current` branch----"
 git commit -a -m commitMsg
 # shellcheck disable=SC2181
 if [ $? -eq 0 ]; then
-    echo "success"
+    echo "---git commit success!---"
 else
-    echo "fail"
+    echo "---git commit FAILED!!!---"
     exit 0
 fi
-#git push origin android-sdk:android-sdk
-#echo "-----push code success----"
-#echo "-----create new release->$releaseTag----"
-## shellcheck disable=SC2086
-#git tag -a $releaseTag -m "$commitMsg"
-## shellcheck disable=SC2086
-#git push origin $releaseTag
-#echo "-----create release->$releaseTag success!----"
+git push origin android-sdk:android-sdk
+# shellcheck disable=SC2181
+if [ $? -eq 0 ]; then
+    echo "---push code success!---"
+else
+    echo "---push code FAILED!!!---"
+    exit 0
+fi
+echo "-----ready to create new release->$releaseTag----"
+# shellcheck disable=SC2086
+git tag -a $releaseTag -m "$commitMsg"
+# shellcheck disable=SC2181
+if [ $? -eq 0 ]; then
+    echo "---create tag success!---"
+else
+    echo "---create tag FAILED!!!---"
+    exit 0
+fi
+# shellcheck disable=SC2086
+git push origin $releaseTag
+# shellcheck disable=SC2181
+if [ $? -eq 0 ]; then
+    echo "---create release->$releaseTag success!---"
+else
+    echo "---push tag FAILED!!!---"
+    exit 0
+fi
